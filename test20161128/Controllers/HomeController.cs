fef01ac2059c4299e771edef10146e2818db00ca
tzuyu <tzuyu@umich.edu>
2016-11-28 11:53:02 -0500

    change mono to mono1

--- test20161128/Controllers/HomeController.cs
+++ test20161128/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace test20161128.Controllers
 			var isMono = Type.GetType("Mono.Runtime") != null;
 
 			ViewData["Version"] = mvcName.Version.Major + "." + mvcName.Version.Minor;
-			ViewData["Runtime"] = isMono ? "Mono" : ".NET";
+			ViewData["Runtime"] = isMono ? "Mono1" : ".NET2";
 
 			return View();
 		}
